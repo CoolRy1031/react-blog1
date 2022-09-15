b77d67723a01d0ab23c94051b0d9c18b9c72f445
CoolRy <that007guy@hotmail.com>
2022-09-15 11:48:14 -0400

    nav bar set up

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = ({ user, handleLogout }) => {
+  return (
+    <>
+    <header className='App-header'>
+      {user ?
+        <nav>
+            Welcome, {user.name}
+            <Link to="/">Posts</Link>
+            <Link to="" onClick={handleLogout}>LOG OUT</Link>
+            <Link to="/changePassword">Change Password</Link>
+        </nav>
+      :
+        <nav>
+            <Link to="/login">Log In</Link>
+            <Link to="/signup">Sign Up</Link>
+        </nav>
+      }
+    </header>
+    </>
+  )
+}
+
+export default NavBar
